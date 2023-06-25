ff39122339ef7b1853ae01140a15059252e7ade9
Akshansh Gusain <akshanshgusain@gmail.com>
2023-06-25 21:58:01 +0530

    feat: added fn to print tcp stream

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
+    let listner = TcpListener::bind("127.0.0.1:9000").unwrap();
 
+    for stream in listner.incoming() {
+        let stream: TcpStream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("request: {}", String::from_utf8_lossy(&buffer[..]));
 }
