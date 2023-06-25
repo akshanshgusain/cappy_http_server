f1af945a3823a788807faccba888268ce433a86b
Akshansh Gusain <akshanshgusain@gmail.com>
2023-06-25 22:42:36 +0530

    feat: returning html response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::Read;
+use std::fs::File;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -14,4 +15,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let mut html_file = File::open("index.html").unwrap();
+    let mut contents = String::new();
+    html_file.read_to_string(&mut contents).unwrap();
+
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
